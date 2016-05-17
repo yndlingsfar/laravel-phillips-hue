6b3ed206d6ef1f10f11de9f2bd0dcfe41973e16c
dsteiner <steiner@auxmoney.com>
2016-05-17 16:36:15 +0200

    implement LightSwitchServiceProvider

--- src/LightSwitchServiceProvider.php
+++ src/LightSwitchServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace Dsteiner23\LightSwitch;
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use DSteiner23\Light\Bridge;
+use DSteiner23\Light\CommunicationInterface;
+use DSteiner23\Light\HueCommunication;
+use DSteiner23\Light\LightSwitch;
+use DSteiner23\Light\LightSwitchInterface;
 use Illuminate\Support\ServiceProvider;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
 
 /**
  * Class LightSwitchServiceProvider
@@ -12,6 +20,39 @@ class LightSwitchServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // TODO: Implement register() method.
+        //Doctrine Annotation Reader registration
+        AnnotationRegistry::registerLoader('class_exists');
+
+        $this->app->singleton(Serializer::class, function ($app) {
+            return SerializerBuilder::create()->build();
+        });
+
+        // @todo: read from configuration
+        $this->app->bind(Bridge::class, function ($app) {
+            return new Bridge(
+                $app['config']->get('phillipsHue.ip'),
+                $app['config']->get('phillipsHue.username')
+            );
+        });
+
+        // Interface binding
+        $this->app->bind(LightSwitchInterface::class, LightSwitch::class);
+        $this->app->bind(CommunicationInterface::class, HueCommunication::class);
+    }
+
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/phillipsHue.php';
+        $this->publishes([$configPath => $this->getConfigPath()], 'config');
+    }
+
+    /**
+     * Get the config path
+     *
+     * @return string
+     */
+    protected function getConfigPath()
+    {
+        return config_path('phillipsHue.php');
     }
 }
