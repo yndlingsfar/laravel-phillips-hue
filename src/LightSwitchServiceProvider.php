b16a2acc57a4cb10ebbc4a904ac760e335d2d046
dsteiner <steiner@auxmoney.com>
2016-05-17 14:27:33 +0200

    adjusted directory structure

---
+++ src/LightSwitchServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dsteiner23\LightSwitch;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class LightSwitchServiceProvider
+ * @package Dsteiner23\LightSwitch
+ */
+class LightSwitchServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
